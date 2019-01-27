05a4a92c3f574bac6dde543a34b145ebff5cc1ff
lmedson <medson-mbp@hotmail.com>
2019-01-27 17:26:48 -0300

    add initial example

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
-const puppeteer = require('puppeteer');
+const express = require('express')
+const app = express()
 
-async function getPic() {
-  const browser = await puppeteer.launch({headless: false});
-  const page = await browser.newPage();
+const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken') 
+const randtoken = require('rand-token') 
 
-  await page.goto('https://www.google.com.br/search?q=dolar&oq=dolar&aqs=chrome..69i57j69i60l5.1131j0j4&sourceid=chrome&ie=UTF-8');
+const refreshTokens = {} 
+const SECRET = "SECRETO_PARA_ENCRIPTACION" 
+app.use(bodyParser.json()) 
+app.use(bodyParser.urlencoded({ extended: true })) 
 
-  const value = await page.evaluate(async () => {
-    let result = await document.querySelector('.VgAgW .nRbRnb .dDoNo.vk_bk.gsrt .DFlfde').innerText;
-  
-    return {
-      result
+app.post('/login', function (req, res, next) { 
+  const username = req.body.username 
+  const password = req.body.password
+  const user = { 
+    'username': username, 
+    'role': 'admin' 
+  } 
+  const token = jwt.sign(user, SECRET, { expiresIn: 300 }) 
+  const refreshToken = randtoken.uid(256) 
+  refreshTokens[refreshToken] = username
+  res.json({token: 'JWT ' + token, refreshToken: refreshToken}) 
+});
+
+app.post('/token', function (req, res, next) {
+  const username = req.body.username
+  const refreshToken = req.body.refreshToken
+  if((refreshToken in refreshTokens) && (refreshTokens[refreshToken] == username)) {
+    const user = {
+      'username': username,
+      'role': 'admin'
     }
-  });
-  //console.log(result.a)
-  await browser.close();
-}
+    const token = jwt.sign(user, SECRET, { expiresIn: 300 })
+    res.json({token: 'JWT ' + token})
+  }
+  else {
+    res.send(401)
+  }
+})
+
+app.listen(3000,()=>console.log('running on 3000'))
